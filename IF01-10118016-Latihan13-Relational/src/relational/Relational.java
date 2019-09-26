b60b9dce8051306a33223ac8b0f9b60b0e8c402c
Rezky Nur Fauzi <55779378+rezkynurfauzi@users.noreply.github.com>
2019-09-26 12:45:12 +0700

    Add files via upload

---
+++ IF01-10118016-Latihan13-Relational/src/relational/Relational.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package relational;
+
+/**
+ *
+ * Nama : Rezky Nur Fauzi
+ * NIM  : 10118016  
+ * Kelas: IF-1 
+ * Deskripsi Program : Relation pada sebuah Java
+ */
+public class Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int bil1 = 10;
+        int bil2 = 20;
+        
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
